37bdc4d19fd34489e44ac1f45f83605e22713f8c
Mirko Häberlin <mirko.haeberlin@gmail.com>
2024-01-12 11:18:54 +0100

    Changed author informations and version

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name='mcp23017',
-    version='0.1.0',
+    version='1.0.0',
     description='MCP23017 Library',
     long_description=open("README.md", 'r').read(),
     long_description_content_type='text/markdown',
     author='Mirko Haeberlin',
-    author_email='mirko.haeberlin@sensorberg.com',
-    url='https://github.com/sensorberg-dev/MCP23017-python',
-    packages=find_packages(exclude=('tests', 'docs'))
+    author_email='mirko.haeberlin@open-things.de',
+    url='https://github.com/open-thngs/MCP23017-python',
+    packages=find_packages(exclude=('test', 'docs')),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
