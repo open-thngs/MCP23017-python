d5480cd9859ddc0a5e4d926b73ebe25a1e91864d
Mirko Häberlin <mirko.haeberlin@gmail.com>
2024-01-22 15:48:08 +0100

    actually add mcp23017 py file to package and increased version number to be 1.0.3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mcp23017',
-    version='1.0.2',
+    version='1.0.3',
     description='MCP23017 Library',
     long_description=open("README.md", 'r').read(),
     long_description_content_type='text/markdown',
     author='Mirko Haeberlin',
     author_email='mirko.haeberlin@open-things.de',
     url='https://github.com/open-thngs/MCP23017-python',
-    packages=find_packages(exclude=('test', 'docs')),
+    packages=['src/mcp23017',],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
